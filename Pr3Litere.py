55a2609e0eb205b476915baaddb19a869743f58e
AlexandruCabac <70658170+AlexandruCabac@users.noreply.github.com>
2020-10-15 18:01:22 +0300

    Update Pr3Litere.py

--- Pr3Litere.py
+++ Pr3Litere.py
@@ -1,6 +1,9 @@
 lit=input()
-Voc=['a','â', 'ă', 'e','i','î', 'o','u','y']
+Voc=['a','A', 'â','Â', 'ă','Ă', 'e','E','i','I','î','Î', 'o','O','u','U','y','Y',]
+Con=['b','B','c','C','d','D','f','F','g','G','h','H','j','J','k','K','l','L','m','M','n','N','p','P','q','Q','r','R','s','S','ș','Ș','t','T','ț','Ț','v','V','w','W','x','X','z','Z']
 if(lit in Voc):
     print("Vocală")
-else:
+elif(lit in Con):
     print("Consoană")
+else:
+    print("NU e literă")
